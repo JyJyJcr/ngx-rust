e5166d8b622eeafea91007ff110a55a613cde497
JyJyJcr <82190170+JyJyJcr@users.noreply.github.com>
2025-01-01 02:19:10 +0900

    fix: add m`odule_new` module

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,10 +1,12 @@
 mod conf;
 mod module;
+mod module_new;
 mod request;
 mod status;
 mod upstream;
 
 pub use conf::*;
 pub use module::*;
+pub use module_new::*;
 pub use request::*;
 pub use status::*;
